741882fcb6e4d9bd123c11f70dfe2af65fe88fc4
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-10-08 19:31:27 +0300

    modifying the dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();  
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Kevin Mulwa is the champion'
+document.body.appendChild(newHeader);
